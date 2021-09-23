e5470ed65ade9853f25e1035eb4879526ae709b7
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-23 17:21:43 +0300

    Create Translate.py

---
+++ Translate.py
@@ -0,0 +1,15 @@
+#in order to install you need to type pip install translators
+import time
+
+
+t1 = time.time()
+
+import translators as ts
+
+text = 'cheese'
+
+print(ts.bing(text, from_language ='en', to_language = 'fr'))
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
