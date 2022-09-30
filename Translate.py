577b9fcc5129103579c1e5648a37095bac2491a7
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-09-30 09:34:20 +0300

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -1,17 +1,6 @@
 #Install translators module with pip install translators
-import time
-time.sleep(6)
-import antigravity
-
-
-t1 = time.time()
-
 import translators as ts
 
 text = 'cheese'
 
 print(ts.bing(text, from_language ='en', to_language = 'fr'))
-
-t2 = time.time()
-t = t2 - t1
-print("Elapsed time is : ", t, " seconds")
